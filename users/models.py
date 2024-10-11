4ad18a6aaa1bcfdff88540a29d365499848a88b1
Arnatech <anoldtech64@gmail.com>
2024-10-11 13:36:03 +0000

    rename

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
+from django.conf import settings
 
 class UserManager(BaseUserManager):
 #Create a regular user
